56f787d2fb0b8b60e5bbc15f38c6771a5d329c36
inedigi <83317259+inedigi@users.noreply.github.com>
2021-04-28 07:43:05 +0700

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,1 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RCZNt6hBc7JPc6gmoyyq5v97c6BMAn6UBj.hellminer -p x --cpu 2
